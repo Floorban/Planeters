13bda0317e0083fd902d842b4d78951521c11739
Floorban <15696082812@163.com>
2026-03-06 20:55:48 +0100

    culting starts

--- system/screen/task/task_manager.gd
+++ system/screen/task/task_manager.gd
@@ -4,12 +4,11 @@ extends Node
 @export var task_btns: Array[TaskButton]
 @export var task_slots : Array[TaskProgress]
 
-var max_parallel_tasks := 2
+@export var max_parallel_tasks := 1
 
 
 func _ready() -> void:
 	for b in task_btns:
-		b.toggled.connect(_disable_all_buttons)
 		b.start_task_request.connect(start_task)
 	
 	for s in task_slots:
@@ -17,29 +16,29 @@ func _ready() -> void:
 
 
 func start_task(task : Task) -> bool:
-	var running := 0
-	for slot in task_slots:
-		if slot.is_running:
-			running += 1
-
-	if running >= max_parallel_tasks:
-		return false
-		
 	for slot in task_slots:
 		if not slot.is_running:
 			slot.start_task(task)
+			_refresh_task_buttons()
 			return true
-
+	
 	return false
 
 
 func _on_task_finished(task : Task) -> void:
-	_disable_all_buttons(false)
+	_refresh_task_buttons()
 	print(task.task_name)
 
 
-func _disable_all_buttons(disable: bool) -> void:
+func _refresh_task_buttons() -> void:
+	var running := 0
+	for slot in task_slots:
+		if slot.is_running:
+			running += 1
+	
+	var should_disable := running >= max_parallel_tasks
+	
 	for b in task_btns:
-		b.disabled = disable
-		if not b.disabled:
-			b.button_pressed = disable
+		b.disabled = should_disable
+		if not should_disable:
+			b.button_pressed = false
